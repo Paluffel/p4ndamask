a5783ac667092801710cec1a88bab0f7e5d5e352
Paluffel <40839103+Paluffel@users.noreply.github.com>
2019-08-07 20:46:33 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -131,7 +131,7 @@ libraryActors.push(MRE.Actor.CreateFromLibrary(this.context, {
                     actor: {
                         name: 'Cube',
                         transform: {local:{
-                        position: { x: 22, y: -1.5, z: 3.7 },
+                        position: { x: 22, y: 0, z: -2 },
                          scale: { x: 0.8, y: 0.8, z: 0.8}
                         }}
                     }
